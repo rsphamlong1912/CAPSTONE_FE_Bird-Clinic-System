5eae9559c7d64306cec4d04879061a5534f36f34
phamlong1912 <rsphamlong1912@gmail.com>
2023-11-04 07:36:50 +0700

    staff checkin and status

--- src/pages/staff/Checkin.js
+++ src/pages/staff/Checkin.js
@@ -4,32 +4,131 @@ import useCurrentDate from "../../hooks/useCurrentDate";
 import LoadingSkeleton from "../../components/loading/LoadingSkeleton";
 import { useNavigate } from "react-router-dom";
 import { api } from "../../services/axios";
+import { confirmAlert } from "react-confirm-alert";
+import { toast } from "react-toastify";
 
 const Checkin = () => {
   const navigate = useNavigate();
   const [customerList, setCustomerList] = useState([]);
   const [loading, setLoading] = useState(true);
+  const { currentDate } = useCurrentDate();
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await api.get("/booking");
-
-        const allBookings = response.data.data;
-        const checkedInBookings = allBookings.filter(
-          (booking) => booking.status === "checked_in"
+        const filterBookings = response.data.data.filter(
+          (booking) =>
+            booking.status === "booked" || booking.status === "check_in"
         );
-        setCustomerList(checkedInBookings);
+        setCustomerList(filterBookings);
       } catch (error) {
         console.log(error);
       }
     };
-
     setTimeout(() => {
       setLoading(false);
     }, 850);
     fetchData();
-  }, []);
-  const { currentDate } = useCurrentDate();
+  });
+
+  const options = {
+    title: "Xác nhận check-in",
+    message: "Tiến hành checkin cuộc hẹn này?",
+    buttons: [
+      {
+        label: "Xác nhận",
+      },
+      {
+        label: "Huỷ",
+      },
+    ],
+    closeOnEscape: true,
+    closeOnClickOutside: true,
+    keyCodeForClose: [8, 32],
+    willUnmount: () => {},
+    afterClose: () => {},
+    onClickOutside: () => {},
+    onKeypress: () => {},
+    onKeypressEscape: () => {},
+    overlayClassName: "overlay-custom-class-name",
+  };
+
+  const handleConfirmAlert = (item) => {
+    const updatedOptions = {
+      ...options,
+      buttons: [
+        {
+          label: "Xác nhận",
+          onClick: async () => {
+            try {
+              const response = await api.put(`/booking/${item.booking_id}`, {
+                status: "check_in",
+              });
+              toast.success("Check-in thành công!", {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+              });
+              console.log("response doi status ne", response.data);
+              // setCustomerList(response.data.data);
+              createNewServiceForm(item);
+            } catch (error) {
+              console.log(error);
+            }
+          },
+        },
+        {
+          label: "Huỷ",
+          onClick: () => {
+            console.log("click no");
+          },
+        },
+      ],
+    };
+    confirmAlert(updatedOptions);
+  };
+
+  const createNewServiceForm = async (item) => {
+    try {
+      // Tạo service_Form
+      const createdResponse = await api.post(`/service_Form/`, {
+        bird_id: item.bird_id,
+        booking_id: item.booking_id,
+        reason_referral: "any",
+        status: "pending",
+        date: item.arrival_date,
+        veterinarian_referral: item.veterinarian_id,
+        total_price: 50000,
+        qr_code: "any",
+        num_ser_must_do: 1,
+        num_ser_has_done: 0,
+        arr_service_pack: [
+          {
+            service_package_id: "SP1",
+            note: "null",
+          },
+        ],
+      });
+      const createdBill = await api.post(`/bill/`, {
+        title: "Thanh toán lần 1",
+        total_price: createdResponse.data.data.total_price,
+        service_form_id: createdResponse.data.data.service_form_id,
+        booking_id: item.booking_id,
+        payment_method: "cash",
+        paypal_transaction_id: "any",
+        status: "any",
+      });
+      console.log("create new bill:", createdBill);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
     <div className={styles.container}>
       <div className={styles.headerContent}>
@@ -67,13 +166,13 @@ const Checkin = () => {
             customerList.map((item, index) => (
               <tr
                 key={index}
-                onClick={() => navigate(`/examing/${item.booking_id}`)}
+                // onClick={() => navigate(`/examing/${item.booking_id}`)}
               >
                 <td> {index + 1} </td>
                 <td>{item.customer_name}</td>
                 <td>Sáo nâu</td>
                 <td>Khám tổng quát</td>
-                <td>{item.checkin_time}</td>
+                <td>{item.estimate_time}</td>
                 <td></td>
                 <td>
                   <strong>Phạm Ngọc Long</strong>
@@ -89,7 +188,16 @@ const Checkin = () => {
                 </td>
 
                 <td>
-                  <div className={styles.btnCheckin}>Check in</div>
+                  {item.status === "booked" ? (
+                    <div
+                      className={styles.btnCheckin}
+                      onClick={() => handleConfirmAlert(item)}
+                    >
+                      Check in
+                    </div>
+                  ) : (
+                    <div className={styles.btnCheckin}>Xem</div>
+                  )}
                 </td>
               </tr>
             ))}
